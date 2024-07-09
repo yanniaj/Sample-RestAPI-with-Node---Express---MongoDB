f12a6a4270b3b030cbd7a2ccc30a75cbb4004a74
Yanni AJ <yanni.a.john@gmail.com>
2024-07-09 19:29:00 +0530

    Update delete and path methods

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Subscriber = require("../models/subscriber");
+const subscriber = require("../models/subscriber");
 
 //Getting all
 router.get("/", async (req, res) => {
@@ -13,8 +14,8 @@ router.get("/", async (req, res) => {
 });
 
 //Getting one
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber.name);
 });
 
 //Creatng one
@@ -32,9 +33,44 @@ router.post("/", async (req, res) => {
 });
 
 //Updating one
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //Deleting one
-router.delete("/", (req, res) => {});
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted subscriber." });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber === null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
