ef0d3f6402f4ca675a9451c8eebbc57b0be3b7c4
Yanni AJ <yanni.a.john@gmail.com>
2024-07-09 18:44:37 +0530

    Add basic get and post req code

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
+
+//Getting all
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//Getting one
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+//Creatng one
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+//Updating one
+router.patch("/:id", (req, res) => {});
+
+//Deleting one
+router.delete("/", (req, res) => {});
 
 module.exports = router;
